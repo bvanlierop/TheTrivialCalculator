66af88559a87df267a1133d874cf8404e76ee2f4
Bart van Lierop <b.vanlierop@additiveindustries.com>
2020-09-29 09:21:57 +0200

    Create buildoutput folder on first run.

--- RunCoverage.bat
+++ RunCoverage.bat
@@ -3,4 +3,5 @@ REM Run Code Coverage Analysis (OpenCover)
 set SOLUTION_DIR=%~dp0
 set COVERAGE_TOOL=%SOLUTION_DIR%\packages\OpenCover.4.6.519\tools\OpenCover.Console.exe
 set OUTPUT_FILES_DIR=%SOLUTION_DIR%\buildoutput
+mkdir "%OUTPUT_FILES_DIR%"
 "%COVERAGE_TOOL%" -target:%SOLUTION_DIR%\RunTests.bat -register:user -output:"%OUTPUT_FILES_DIR%\CodeCoverageResults.xml"
