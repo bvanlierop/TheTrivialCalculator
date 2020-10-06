a3ae0b7dee8738d743fb81803caf9686136106b2
Bart van Lierop <b.vanlierop@additiveindustries.com>
2020-10-06 11:27:54 +0200

    bla

--- RunCoverage.bat
+++ RunCoverage.bat
@@ -4,4 +4,5 @@ set SOLUTION_DIR=%~dp0
 set COVERAGE_TOOL=%SOLUTION_DIR%\packages\OpenCover.4.6.519\tools\OpenCover.Console.exe
 set OUTPUT_FILES_DIR=%SOLUTION_DIR%\buildoutput
 mkdir "%OUTPUT_FILES_DIR%"
+
 "%COVERAGE_TOOL%" -target:%SOLUTION_DIR%\RunTests.bat -register:user -output:"%OUTPUT_FILES_DIR%\CodeCoverageResults.xml"
